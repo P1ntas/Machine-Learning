8489ab65d0b78ab1143a165cbfa30e402fde1bba
Juanbl2001 <juan.bellon2001@hotmail.com>
2023-09-21 15:16:51 +0100

    template

---
+++ data_preprocessing.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+def load_data():
+    # Load all your CSV files into Pandas DataFrames
+    return
+
+def merge_data():
+    # Merge the datasets into a single DataFrame using keys like 'year', 'tmID', etc.
+    return
+
+def clean_data():
+    # Handle missing or incomplete data
+    return
+
+def feature_engineering():
+    # Generate new features that can improve the prediction
+    return
+
+if __name__ == "__main__":
+    # Load Data
+    load_data()
+    
+    # Merge Data
+    merge_data()
+    
+    # Data Cleaning
+    clean_data()
+    
+    # Feature Engineering
+    feature_engineering()
+    
+    # Save the cleaned, merged, and engineered data to a new CSV
+    # merged_and_cleaned_data.to_csv("preprocessed_data.csv", index=False)
